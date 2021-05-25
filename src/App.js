7be4b8b59bb6813a9bf2c3272f3e9825f7eaf345
poomsombutboon <poompudd@gmail.com>
2021-05-25 11:48:12 +0700

    map key to sound

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import TextEditor from './components/TextEditor'
-import Demo from './components/Demo'
 
 function App() {
   return (
     <div className="App">
       <TextEditor />
-      {/* <Demo url={'../../assets/sounds/bubbles.mp3'} /> */}
     </div>
   );
 }
