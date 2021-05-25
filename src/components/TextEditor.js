c8227e7da38054dd562f6e51168c362ef153cfa5
poomsombutboon <poompudd@gmail.com>
2021-05-25 09:57:37 +0700

    add event handler

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,21 +1,43 @@
 import { useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import useSound from 'use-sound';
-import audio from '../assets/comfort.mp3'
+import keyboardSound from '../assets/comfort.mp3'
 
 function TextEditor(){
     const [text, setText] = useState('');
     
-    const [playSound] = useSound(
-        audio
+    const playSound = (e) => {
+        // const key = e.target.value;
+        console.log(e);
+        if (e.key === 'a') {
+            playKeyboardSound();
+        } else {
+            alert('What are you doing!');
+        }
+    }
+    const [playKeyboardSound] = useSound(
+        keyboardSound
     );
 
     return (
-        <div>
+        <div >
+            {/* <CKEditor
+                editor={ClassicEditor}
+                data={text}
+                onChange={(e, editor) => {
+                    const data = editor.getData();
+                    // console.log(editor);
+                    setText(data);
+                    // editor.on('key', playSound(e))
+                }}
+            /> */}
             <input 
                 type='text' 
                 placeholder='Type Something!'
                 value = {text}
                 onChange={(e) => setText(e.target.value)}
+                onKeyPress={(e) => playSound(e)}
             />
             <button onClick={playSound}>
                 click me 
